048c10d6a06c12e204d7330fb6142729d5848192
Loadszz <66636253+Loadszz@users.noreply.github.com>
2024-09-03 13:56:10 +0300

    Update _app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,9 @@
 import '@/styles/style.css'
+import Head from 'next/head';
 
 export default function App({ Component, pageProps }) {
+	<Head>
+        	<title>Weather</title>
+      	</Head>
 	return <Component {...pageProps} />
 }
